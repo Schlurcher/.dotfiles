d65a903e70c60129d34a4d45b5b8458ac6b0fb26
root <root@gitlab.alsix.de>
2021-02-25 14:50:06 +0100

    install pl10k

--- pre-install.sh
+++ pre-install.sh
@@ -5,3 +5,11 @@ if [ ! -d "$OMZDIR" ]; then
     else
 	/bin/zsh -i -c "omz update"
 fi
+
+# Check if Powerlevel10k is installed
+PL10KDIR="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+if [ ! -d "$PL10KDIR" ]; then
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $PL10KDIR
+fi
+	
+
